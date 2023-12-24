60ac4b4bb0efb576acb53c9c46830c647499fc00
So Cool <127.0.0.1.pm@gmail.com>
2023-12-24 11:45:59 +0700

    add generic type and compose function

--- compose-demo.ts
+++ compose-demo.ts
@@ -12,3 +12,13 @@ type IncrementThenToString = (x: number) => string
 const increment_then_tostring: IncrementThenToString = x => tostring(increment(x))
 
 console.log(increment_then_tostring(6))
+
+type Compose = <A,B,C>(
+    f: (x: B) => C, 
+    g: (x: A) => B
+) => (x: A) => C
+const compose: Compose = (f,g) => x => f(g(x))
+
+const increment_then_tostring2: IncrementThenToString = compose(tostring, increment)
+
+console.log(increment_then_tostring2(6))
