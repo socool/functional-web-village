f2a27b1f271912b6a58caa2d837f98f71fb25929
So Cool <127.0.0.1.pm@gmail.com>
2023-12-24 11:35:44 +0700

    combine function

--- compose-demo.ts
+++ compose-demo.ts
@@ -2,3 +2,13 @@ type Increment = (x: number) => number
 const increment: Increment = x => x+1
 
 console.log(increment(2))
+
+type Tostring = (x: number) => string
+const tostring: Tostring = x => `"${x}"`
+
+console.log(tostring(2))
+
+type IncrementThenToString = (x: number) => string
+const increment_then_tostring: IncrementThenToString = x => tostring(increment(x))
+
+console.log(increment_then_tostring(6))
