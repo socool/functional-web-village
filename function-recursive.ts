81db36b91311633b2335b33b75ee37f860a9161f
Kamolsit Mongkolsrisawat <127.0.0.1.pm@gmail.com>
2023-12-25 06:32:02 +0000

    code: sum_all, sum_all2

--- function-recursive.ts
+++ function-recursive.ts
@@ -17,3 +17,19 @@ function normal_sum_all(xs: number[]): number{
 }
 
 console.log(normal_sum_all([1,2,3]))
+
+type SumAll = (xs: number[]) => number
+const sum_all: SumAll = xs=>{
+    if(xs.length === 0){
+        return 0
+    }
+
+    const [head, ...rest] = xs
+    return head + sum_all(rest)
+}
+
+console.log(sum_all([1,2,3,4]))
+
+const sum_all2: SumAll = xs => (xs.length === 0) ? 0 : xs[0] + sum_all2(xs.slice(1))
+
+console.log(sum_all2([1,2,3,4]))
