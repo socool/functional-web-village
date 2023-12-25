222765448ee18ac880291ed442f4eeec0134bc71
Kamolsit Mongkolsrisawat <127.0.0.1.pm@gmail.com>
2023-12-25 01:18:07 +0000

    add curying demo 2

--- currying-demo.ts
+++ currying-demo.ts
@@ -10,11 +10,28 @@ console.log(normal_sum(1,2))
 // }
 
 type Sum = (a: number) => (b: number) => number
-const sum2: Sum = a => b => a+b
+const f_sum: Sum = a => b => a + b
 
-console.log(sum2(1)(2))
+console.log(f_sum(1)(2))
 
-const inc: Increment = sum2(1)
+type Inc = (x: number) => number
+const inc: Inc = f_sum(1)
+const inc2: Inc = a => f_sum(a)(1)
 
+console.log(inc(5),":",inc2(5))
 
-console.log(sum2(1)(2))
+type Dec = (x: number) => number
+const dec: Dec = f_sum(-1)
+
+console.log(dec(6))
+
+type Curry2 = (f: (a: number, b: number) => number)
+    => (a: number)
+    => (b: number)
+    => number
+    
+const curry2:Curry2 = f => a => b => f(a,b)
+
+const f_sum2 = curry2(normal_sum)
+
+console.log(f_sum2(1)(2))
