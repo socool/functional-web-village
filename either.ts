0a152ec8adf83063f25a56cca8fb4297827d12e5
Kamolsit Mongkolsrisawat <127.0.0.1.pm@gmail.com>
2023-12-26 14:29:41 +0000

    add composed

--- either.ts
+++ either.ts
@@ -1,3 +1,4 @@
+import {compose} from './compose-demo'
 function divideTwoIfEven(num: number): number{
     if(num === 0){
         throw 'cannot divide by zero'
@@ -31,6 +32,8 @@ const right = <E, A>(a: A): Either<E, A> =>({
     right: a
 })
 
+const isLeft = <E, A>(x: Either<E, A>): x is Left<E> => x._tag === 'Left'
+
 function divideTwoIfEven2(num: number): Either<string, number>{
     if(num === 0){
         return left('cannot divide by zero')
@@ -44,3 +47,15 @@ function divideTwoIfEven2(num: number): Either<string, number>{
 console.log(divideTwoIfEven2(8))
 console.log(divideTwoIfEven2(0))
 console.log(divideTwoIfEven2(3))
+
+type Increment = (x: number) => number
+const increment: Increment = x => x+1
+
+const composed = compose(
+    x => isLeft(x) ? x : right(increment(x.right)), 
+    divideTwoIfEven2
+)
+
+console.log(composed(8))
+console.log(composed(0))
+console.log(composed(3))
