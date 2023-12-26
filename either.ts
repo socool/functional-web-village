d09554f8c58b576b2a987b39879464085bd1e1fe
Kamolsit Mongkolsrisawat <127.0.0.1.pm@gmail.com>
2023-12-26 14:23:55 +0000

    add divideTwoIfEven2

--- either.ts
+++ either.ts
@@ -9,4 +9,38 @@ function divideTwoIfEven(num: number): number{
 }
 
 console.log(divideTwoIfEven(8))
-console.log(divideTwoIfEven(3))
+// console.log(divideTwoIfEven(3))// need to convert to total function
+
+type Either<E,A> = Left<E> | Right<A>
+interface Left<E> {
+    _tag: 'Left'
+    left: E
+}
+interface Right<A> {
+    _tag: 'Right'
+    right: A
+}
+
+const left = <E, A>(e: E): Either<E, A> => ({
+    _tag: 'Left',
+    left: e
+})
+
+const right = <E, A>(a: A): Either<E, A> =>({
+    _tag: 'Right',
+    right: a
+})
+
+function divideTwoIfEven2(num: number): Either<string, number>{
+    if(num === 0){
+        return left('cannot divide by zero')
+    }else if(num % 2 !== 0){
+        return left('num is not even')
+    }else{
+        return right(2/num)
+    }
+}
+
+console.log(divideTwoIfEven2(8))
+console.log(divideTwoIfEven2(0))
+console.log(divideTwoIfEven2(3))
