39876ec3c74d04ab81c087432c5813b70ad9b37d
Kamolsit Mongkolsrisawat <127.0.0.1.pm@gmail.com>
2023-12-27 09:36:07 +0000

    add option

--- pattern-matching.ts
+++ pattern-matching.ts
@@ -1,6 +1,16 @@
-import {Option, None, none, Some, some, isNone} from './Option.ts'
+import {Option, None, none, Some, some, isNone} from './Option'
 
 // Option
 type Match = <A,B>(onNone: () => B, onSome: (a: A) => B)
 => (x: Option<A>) => B
 
+const match: Match = (onNone, onSome) => x => 
+    isNone(x) ? onNone() : onSome(x.value)
+
+const mayBeNum: Option<number> = some(12)
+const result = match(
+    () => `num does not exist`,
+    (a: number) => `num is ${a}`
+)(mayBeNum)
+
+console.log(result)
