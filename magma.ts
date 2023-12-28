95436aa04d3d2a204744c6d4af800dd6db15814b
Kamolsit Mongkolsrisawat <127.0.0.1.pm@gmail.com>
2023-12-28 09:54:50 +0000

    change addAll to point free form style

--- magma.ts
+++ magma.ts
@@ -2,10 +2,10 @@ import {List, cons, nil} from './list'
 import {matchL} from './pattern-matching'
 
 type AddAll = (xs: List<number> )=> number
-const addAll: AddAll = xs =>
+const addAll: AddAll = 
     matchL(
         () => 0,
         (head:number, tail: List<number>) => head + addAll(tail)
-    )(xs)
+    )
 
 console.log(addAll(cons(2,cons(3,cons(4,nil)))))
